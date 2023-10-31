a046f386386003bfe1f3955bcc3cabf63b1f29da
Rafael <rafael.soares.neves@gmail.com>
2023-10-31 19:49:51 -0300

    funcionando

--- 04-greeting-service/src/main/java/com/rafa/greetingservice/controller/HelloWorldController.java
+++ 04-greeting-service/src/main/java/com/rafa/greetingservice/controller/HelloWorldController.java
@@ -1,25 +1,27 @@
 package com.rafa.greetingservice.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.rafa.greetingservice.configuration.GreetingConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
 
+    @Autowired
+    GreetingConfiguration greetingConfiguration;
+
 /*  @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World";
     }*/
 
-    @Value("${greeting-service.default-value}")
-    private String valorParam;
+    //@Value("${greeting-service.default-value}")
+    //private String valorParam;
 
     @RequestMapping("/hello")
     public String helloWorld(){
-        return "Hello World: "+ valorParam;
+        return "Hello World: "+ greetingConfiguration.getDefaultValue();
     }
 }
